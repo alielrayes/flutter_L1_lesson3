31f89b584570cf3ea36d2149b3d7d6eb3e34bd27
alielrayes <engali7assan@yahoo.com>
2023-09-05 00:52:32 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(useMaterial3: true),
+      home: SimpleProject(),
+    );
+  }
+}
+
+class SimpleProject extends StatelessWidget {
+  const SimpleProject({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 10,
+        title: Text("Facebook"),
+        centerTitle: true,
+        leading: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.menu,
+              size: 33,
+            )),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.message,
+                size: 25,
+              )),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.search,
+                size: 25,
+              )),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          "Hello, Ali 👋",
+          style: TextStyle(fontSize: 40),
+        ),
+      ),
+    );
+  }
+}
